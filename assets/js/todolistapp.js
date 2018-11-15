f31345fbafb4a669bed2eb9e65477b738cbd568b
Thomas Kearney <tom.f.kearney@gmail.com>
2018-11-15 14:16:05 -0800

    add new items to list via the input box and enter key. make sure new items are interactive once added

--- assets/js/todolistapp.js
+++ assets/js/todolistapp.js
@@ -1,13 +1,25 @@
 //toggle text decoration on click (task was completed)
-$("li").click(function()
+$("ul").on("click", "li", function()
 {
     $(this).toggleClass("completedTask");
 });
 
-$("span").click(function()
+$("ul").on("click", "span", function()
 {
     $(this).parent().fadeOut(400, function()
     {
         $(this).remove();
     });
 })
+
+//add new todos when enter is pressed from input 
+$("input[type='text']").keypress(function(event)
+    {
+        if(event.which === 13) //enter key hit
+        {
+            var todoText = $(this).val(); 
+            $("ul").append("<li><span>X</span> " + todoText + "</li>");
+            $(this).val("");
+        }
+    }
+);
